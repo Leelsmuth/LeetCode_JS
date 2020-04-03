e4f3fef387f9663194f171787c98492e5f6b7da3
Leelsmuth <femianjorin@rocketmail.com>
2020-04-04 00:12:21 +0100

    optimized solution for valid palindrome

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,8 +1,25 @@
 function isPalindrome(s) {
-  s.split("")
-  s.split('').reverse()
-  s.split('').reverse().join('')
+  // Sanitize the input string
+  s = s.toLowerCase().replace(/[\W_]/g, "");
 
+  let left = 0
+  let right = s.length - 1 
+  
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false
+    }
+    left++
+    right--
+
+  }
+
+  return true
 }
+
+/* 
+Time Complexity = O(n)
+Space Complexity = O(1)
+*/
 
 module.exports = isPalindrome;
